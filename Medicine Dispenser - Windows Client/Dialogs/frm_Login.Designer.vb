70901db4e8e0269fc0a97e8a06b91d7cacf566c6
vishunupriya7 <priyavishnu462@gmail.com>
2019-01-05 14:50:01 +0530

    Design Login Form
    * Add XtraFormTemp to inherit Devexpress Themes
    * Create Login form from XtraFormTemp

---
+++ Medicine Dispenser - Windows Client/Dialogs/frm_Login.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_Login
+    Inherits XtraFormTemp
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_Login))
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
+        Me.txt_Username = New DevExpress.XtraEditors.TextEdit()
+        Me.txt_Password = New DevExpress.XtraEditors.TextEdit()
+        Me.btn_Login = New DevExpress.XtraEditors.SimpleButton()
+        CType(Me.txt_Username.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txt_Password.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(12, 15)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(55, 13)
+        Me.LabelControl1.TabIndex = 0
+        Me.LabelControl1.Text = "Username :"
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Location = New System.Drawing.Point(14, 41)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(53, 13)
+        Me.LabelControl2.TabIndex = 1
+        Me.LabelControl2.Text = "Password :"
+        '
+        'txt_Username
+        '
+        Me.txt_Username.Location = New System.Drawing.Point(73, 12)
+        Me.txt_Username.Name = "txt_Username"
+        Me.txt_Username.Size = New System.Drawing.Size(219, 20)
+        Me.txt_Username.TabIndex = 2
+        '
+        'txt_Password
+        '
+        Me.txt_Password.Location = New System.Drawing.Point(73, 38)
+        Me.txt_Password.Name = "txt_Password"
+        Me.txt_Password.Properties.UseSystemPasswordChar = True
+        Me.txt_Password.Size = New System.Drawing.Size(219, 20)
+        Me.txt_Password.TabIndex = 3
+        '
+        'btn_Login
+        '
+        Me.btn_Login.Location = New System.Drawing.Point(217, 64)
+        Me.btn_Login.Name = "btn_Login"
+        Me.btn_Login.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Login.TabIndex = 4
+        Me.btn_Login.Text = "Login"
+        '
+        'frm_Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(304, 96)
+        Me.Controls.Add(Me.btn_Login)
+        Me.Controls.Add(Me.txt_Password)
+        Me.Controls.Add(Me.txt_Username)
+        Me.Controls.Add(Me.LabelControl2)
+        Me.Controls.Add(Me.LabelControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frm_Login"
+        Me.Text = "Login"
+        CType(Me.txt_Username.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txt_Password.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents txt_Username As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents txt_Password As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents btn_Login As DevExpress.XtraEditors.SimpleButton
+End Class
