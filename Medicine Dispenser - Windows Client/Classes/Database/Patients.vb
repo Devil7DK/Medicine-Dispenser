2deedf2e5a137825d7675baa4094ad781ed7fd96
Devil7DK <dineshthangavel47@gmail.com>
2019-01-25 09:23:27 +0530

    Database: Patients - Implement New & Update Functions

--- Medicine Dispenser - Windows Client/Classes/Database/Patients.vb
+++ Medicine Dispenser - Windows Client/Classes/Database/Patients.vb
@@ -32,6 +32,65 @@ Namespace Database
 
             Return R
         End Function
+
+        Public Shared Function [New](ByVal Name As String, ByVal Diseases As List(Of String), ByVal Allergies As List(Of String), ByVal Medication As List(Of Medication), ByVal Doctor As Doctor) As Patient
+            Dim R As Patient = Nothing
+            Dim Connection As MySqlConnection = GetConnection()
+
+            Try
+                Dim CommandString As String = String.Format("INSERT INTO {0} (name,diseases,allergies,medication,doctor) VALUES(@name,@diseases,@allergies,@medication,@doctor);SELECT SCOPE_IDENTITY();", TableName)
+                If Connection.State = ConnectionState.Closed Then Connection.Open()
+
+                Using Command As New MySqlCommand(CommandString, Connection)
+                    Command.Parameters.AddWithValue("@name", Name)
+                    Command.Parameters.AddWithValue("@diseases", Utils.Serializer.ToZXML(Diseases))
+                    Command.Parameters.AddWithValue("@allergies", Utils.Serializer.ToZXML(Allergies))
+                    Command.Parameters.AddWithValue("@medication", Utils.Serializer.ToZXML(Medication))
+                    Command.Parameters.AddWithValue("@doctor", Doctor.ID)
+
+                    Dim ID As Integer = Command.ExecuteScalar
+                    If ID > 0 Then
+                        R = New Patient(ID, Name, Diseases, Allergies, Medication, Doctor)
+                    Else
+                        DevExpress.XtraEditors.XtraMessageBox.Show("Unknown error while inserting patient.", "Failed!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                    End If
+                End Using
+            Catch ex As Exception
+                DevExpress.XtraEditors.XtraMessageBox.Show("Error while inserting patient." & vbNewLine & ex.Message, "Failed!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+
+            Return R
+        End Function
+
+        Public Shared Function Update(ByVal UpdatedPatient As Patient) As Boolean
+            Dim R As Boolean = False
+            Dim Connection As MySqlConnection = GetConnection()
+
+            Try
+                Dim CommandString As String = String.Format("UPDATE {0} SET name=@name,diseases=@diseases,allergies=@allergies,medication=@medication,doctor=@doctor WHERE id=@id;", TableName)
+                If Connection.State = ConnectionState.Closed Then Connection.Open()
+
+                Using Command As New MySqlCommand(CommandString, Connection)
+                    Command.Parameters.AddWithValue("@id", UpdatedPatient.ID)
+                    Command.Parameters.AddWithValue("@name", UpdatedPatient.Name)
+                    Command.Parameters.AddWithValue("@diseases", Utils.Serializer.ToZXML(UpdatedPatient.Diseases))
+                    Command.Parameters.AddWithValue("@allergies", Utils.Serializer.ToZXML(UpdatedPatient.Allergies))
+                    Command.Parameters.AddWithValue("@medication", Utils.Serializer.ToZXML(UpdatedPatient.Medication))
+                    Command.Parameters.AddWithValue("@doctor", UpdatedPatient.Doctor.ID)
+
+                    Dim Result As Integer = Command.ExecuteNonQuery
+                    If Result = 1 Then
+                        R = True
+                    Else
+                        DevExpress.XtraEditors.XtraMessageBox.Show("Unknown error while updating patient.", "Failed!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                    End If
+                End Using
+            Catch ex As Exception
+                DevExpress.XtraEditors.XtraMessageBox.Show("Error while updating patient." & vbNewLine & ex.Message, "Failed!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+
+            Return R
+        End Function
 #End Region
 
 #Region "Private Functions"
