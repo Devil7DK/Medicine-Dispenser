b371a3b92670ebd97a3021a9844dcdd01e64353f
Devil7DK <dineshthangavel47@gmail.com>
2019-01-25 08:30:06 +0530

    Objects: Patient - Add Missing Namespace

--- Medicine Dispenser - Windows Client/Classes/Objects/Patient.vb
+++ Medicine Dispenser - Windows Client/Classes/Objects/Patient.vb
@@ -1,42 +1,44 @@
-﻿Public Class Patient
+﻿Namespace Objects
+    Public Class Patient
 
 #Region "Properties/Fields"
-    Property ID As Integer
-    Property Name As String
-    Property Diseases As List(Of String)
-    Property Allergies As List(Of String)
-    Property Medication As List(Of Medication)
+        Property ID As Integer
+        Property Name As String
+        Property Diseases As List(Of String)
+        Property Allergies As List(Of String)
+        Property Medication As List(Of Medication)
 #End Region
 
 #Region "Constructors"
-    Sub New(ByVal ID As Integer, ByVal Name As String, ByVal Diseases As List(Of String), ByVal Allergies As List(Of String), ByVal Medication As List(Of Medication))
-        Me.ID = ID
-        Me.Name = Name
-        Me.Diseases = Diseases
-        Me.Allergies = Allergies
-        Me.Medication = Medication
-    End Sub
+        Sub New(ByVal ID As Integer, ByVal Name As String, ByVal Diseases As List(Of String), ByVal Allergies As List(Of String), ByVal Medication As List(Of Medication))
+            Me.ID = ID
+            Me.Name = Name
+            Me.Diseases = Diseases
+            Me.Allergies = Allergies
+            Me.Medication = Medication
+        End Sub
 
-    Sub New(ByVal ID As Integer, ByVal Name As String, ByVal Diseases As String, ByVal Allergies As String, ByVal Medication As String)
-        Me.ID = ID
-        Me.Name = Name
+        Sub New(ByVal ID As Integer, ByVal Name As String, ByVal Diseases As String, ByVal Allergies As String, ByVal Medication As String)
+            Me.ID = ID
+            Me.Name = Name
 
-        If Diseases.Trim <> "" Then
-            Me.Diseases = Utils.Serializer.FromXML(Of List(Of String))(Diseases)
-        End If
+            If Diseases.Trim <> "" Then
+                Me.Diseases = Utils.Serializer.FromXML(Of List(Of String))(Diseases)
+            End If
 
-        If Allergies.Trim <> "" Then
-            Me.Allergies = Utils.Serializer.FromXML(Of List(Of String))(Allergies)
-        End If
+            If Allergies.Trim <> "" Then
+                Me.Allergies = Utils.Serializer.FromXML(Of List(Of String))(Allergies)
+            End If
 
-        If Medication.Trim <> "" Then
-            Me.Medication = Utils.Serializer.FromXML(Of List(Of Medication))(Medication)
-        End If
+            If Medication.Trim <> "" Then
+                Me.Medication = Utils.Serializer.FromXML(Of List(Of Medication))(Medication)
+            End If
 
-        If Me.Diseases Is Nothing Then Me.Diseases = New List(Of String)
-        If Me.Allergies Is Nothing Then Me.Allergies = New List(Of String)
-        If Me.Medication Is Nothing Then Me.Medication = New List(Of Medication)
-    End Sub
+            If Me.Diseases Is Nothing Then Me.Diseases = New List(Of String)
+            If Me.Allergies Is Nothing Then Me.Allergies = New List(Of String)
+            If Me.Medication Is Nothing Then Me.Medication = New List(Of Medication)
+        End Sub
 #End Region
 
-End Class
+    End Class
+End Namespace
