e439b05fe12348884e52336ef7af47008e1189d7
Devil7DK <dineshthangavel47@gmail.com>
2019-01-25 09:09:34 +0530

    Objects: Medication - Add Missing Namespace

--- Medicine Dispenser - Windows Client/Classes/Objects/Medication.vb
+++ Medicine Dispenser - Windows Client/Classes/Objects/Medication.vb
@@ -1,19 +1,21 @@
-﻿Public Class Medication
+﻿Namespace Objects
+    Public Class Medication
 
 #Region "Properties/Fields"
-    Property Medicine As Medicine
-    Property Dosage As Integer
-    Property Hour As Integer
-    Property Minute As Integer
+        Property Medicine As Medicine
+        Property Dosage As Integer
+        Property Hour As Integer
+        Property Minute As Integer
 #End Region
 
 #Region "Constructor"
-    Sub New(ByVal Medicine As Medicine, ByVal Dosage As Integer, ByVal Hour As Integer, ByVal Minute As Integer)
-        Me.Medicine = Medicine
-        Me.Dosage = Dosage
-        Me.Hour = Hour
-        Me.Minute = Minute
-    End Sub
+        Sub New(ByVal Medicine As Medicine, ByVal Dosage As Integer, ByVal Hour As Integer, ByVal Minute As Integer)
+            Me.Medicine = Medicine
+            Me.Dosage = Dosage
+            Me.Hour = Hour
+            Me.Minute = Minute
+        End Sub
 #End Region
 
-End Class
+    End Class
+End Namespace
