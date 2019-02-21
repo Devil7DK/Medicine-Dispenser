35201e56ef1c5f9bee4272e6fcf62cfb037802dd
Devil7DK <dineshthangavel47@gmail.com>
2019-02-21 19:06:27 +0530

    Objects: Patient - Use Binding List for Lists

--- Medicine Dispenser - Windows Client/Classes/Objects/Patient.vb
+++ Medicine Dispenser - Windows Client/Classes/Objects/Patient.vb
@@ -1,17 +1,19 @@
-﻿Namespace Objects
+﻿Imports System.ComponentModel
+
+Namespace Objects
     Public Class Patient
 
 #Region "Properties/Fields"
         Property ID As Integer
         Property Name As String
-        Property Diseases As List(Of String)
-        Property Allergies As List(Of String)
-        Property Medication As List(Of Medication)
+        Property Diseases As BindingList(Of String)
+        Property Allergies As BindingList(Of String)
+        Property Medication As BindingList(Of Medication)
         Property Doctor As Doctor
 #End Region
 
 #Region "Constructors"
-        Sub New(ByVal ID As Integer, ByVal Name As String, ByVal Diseases As List(Of String), ByVal Allergies As List(Of String), ByVal Medication As List(Of Medication), ByVal Doctor As Doctor)
+        Sub New(ByVal ID As Integer, ByVal Name As String, ByVal Diseases As BindingList(Of String), ByVal Allergies As BindingList(Of String), ByVal Medication As BindingList(Of Medication), ByVal Doctor As Doctor)
             Me.ID = ID
             Me.Name = Name
             Me.Diseases = Diseases
@@ -26,20 +28,20 @@
             Me.Doctor = Doctor
 
             If Diseases IsNot Nothing Then
-                Me.Diseases = Utils.Serializer.FromZXML(Of List(Of String))(Diseases)
+                Me.Diseases = Utils.Serializer.FromZXML(Of BindingList(Of String))(Diseases)
             End If
 
             If Allergies IsNot Nothing Then
-                Me.Allergies = Utils.Serializer.FromZXML(Of List(Of String))(Allergies)
+                Me.Allergies = Utils.Serializer.FromZXML(Of BindingList(Of String))(Allergies)
             End If
 
             If Medication IsNot Nothing Then
-                Me.Medication = Utils.Serializer.FromZXML(Of List(Of Medication))(Medication)
+                Me.Medication = Utils.Serializer.FromZXML(Of BindingList(Of Medication))(Medication)
             End If
 
-            If Me.Diseases Is Nothing Then Me.Diseases = New List(Of String)
-            If Me.Allergies Is Nothing Then Me.Allergies = New List(Of String)
-            If Me.Medication Is Nothing Then Me.Medication = New List(Of Medication)
+            If Me.Diseases Is Nothing Then Me.Diseases = New BindingList(Of String)
+            If Me.Allergies Is Nothing Then Me.Allergies = New BindingList(Of String)
+            If Me.Medication Is Nothing Then Me.Medication = New BindingList(Of Medication)
         End Sub
 #End Region
 
