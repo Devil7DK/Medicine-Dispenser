79148df2cbbb9f61a7ec3db765189ca063443a0d
vishunupriya7 <priyavishnu462@gmail.com>
2019-02-23 13:35:58 +0530

    Objects: Patient - Add 'Photo' Field

--- Medicine Dispenser - Windows Client/Classes/Objects/Patient.vb
+++ Medicine Dispenser - Windows Client/Classes/Objects/Patient.vb
@@ -10,19 +10,21 @@ Namespace Objects
         Property Allergies As BindingList(Of String)
         Property Medication As BindingList(Of Medication)
         Property Doctor As Doctor
+        Property Photo As Image
 #End Region
 
 #Region "Constructors"
-        Sub New(ByVal ID As Integer, ByVal Name As String, ByVal Diseases As BindingList(Of String), ByVal Allergies As BindingList(Of String), ByVal Medication As BindingList(Of Medication), ByVal Doctor As Doctor)
+        Sub New(ByVal ID As Integer, ByVal Name As String, ByVal Diseases As BindingList(Of String), ByVal Allergies As BindingList(Of String), ByVal Medication As BindingList(Of Medication), ByVal Doctor As Doctor, ByVal Photo As Image)
             Me.ID = ID
             Me.Name = Name
             Me.Diseases = Diseases
             Me.Allergies = Allergies
             Me.Medication = Medication
             Me.Doctor = Doctor
+            Me.Photo = Photo
         End Sub
 
-        Sub New(ByVal ID As Integer, ByVal Name As String, ByVal Diseases As Byte(), ByVal Allergies As Byte(), ByVal Medication As Byte(), ByVal Doctor As Doctor)
+        Sub New(ByVal ID As Integer, ByVal Name As String, ByVal Diseases As Byte(), ByVal Allergies As Byte(), ByVal Medication As Byte(), ByVal Doctor As Doctor, ByVal Photo As Byte())
             Me.ID = ID
             Me.Name = Name
             Me.Doctor = Doctor
@@ -39,9 +41,16 @@ Namespace Objects
                 Me.Medication = Utils.Serializer.FromZXML(Of BindingList(Of Medication))(Medication)
             End If
 
+            If Photo IsNot Nothing Then
+                Using MS As New IO.MemoryStream(Photo)
+                    Me.Photo = Image.FromStream(MS)
+                End Using
+            End If
+
             If Me.Diseases Is Nothing Then Me.Diseases = New BindingList(Of String)
             If Me.Allergies Is Nothing Then Me.Allergies = New BindingList(Of String)
             If Me.Medication Is Nothing Then Me.Medication = New BindingList(Of Medication)
+            If Me.Photo Is Nothing Then Me.Photo = My.Resources.patient
         End Sub
 #End Region
 
