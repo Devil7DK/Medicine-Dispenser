217756b45fbfcf600ceeabcee462bff9b3782421
Devil7DK <dineshthangavel47@gmail.com>
2019-02-25 19:02:58 +0530

    Project: Common - Add Speech Synthesize Functions

--- Medicine Dispenser - Windows Client/Classes/Common.vb
+++ Medicine Dispenser - Windows Client/Classes/Common.vb
@@ -1,5 +1,10 @@
 ﻿Module Common
 
+#Region "Variables"
+    Private SpeechSynthesizer As New Speech.Synthesis.SpeechSynthesizer
+#End Region
+
+#Region "Public Functions"
     Function ExInputBox(ByVal Title As String, ByVal Prompt As String, Optional ByVal DefaultValue As String = "") As String
         Dim InputBox As New frm_StringInputBox(Title, Prompt, DefaultValue)
         If InputBox.ShowDialog = DialogResult.OK Then
@@ -18,5 +23,26 @@
                 Return False
         End Select
     End Function
+
+    Function Speak(ByVal Text As String) As Boolean
+        Try
+            SpeechSynthesizer.Rate = 0
+            SpeechSynthesizer.Volume = 100
+            SpeechSynthesizer.SelectVoice(GetVoices(0))
+            SpeechSynthesizer.Speak(Text)
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+    Function GetVoices() As String()
+        Dim R As New List(Of String)
+        For Each Voice As Speech.Synthesis.InstalledVoice In SpeechSynthesizer.GetInstalledVoices(Globalization.CultureInfo.CurrentCulture)
+            R.Add(Voice.VoiceInfo.Name)
+        Next
+        Return R.ToArray
+    End Function
+#End Region
 
 End Module
