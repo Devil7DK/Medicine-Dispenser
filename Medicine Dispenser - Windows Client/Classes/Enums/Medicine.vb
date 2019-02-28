1b5d80f8f2f6a18976a47ff623b204013c167db7
Devil7DK <dineshthangavel47@gmail.com>
2019-02-28 13:17:37 +0530

    Enums: Medicine - Add Namespace

--- Medicine Dispenser - Windows Client/Classes/Enums/Medicine.vb
+++ Medicine Dispenser - Windows Client/Classes/Enums/Medicine.vb
@@ -1,5 +1,7 @@
-﻿Public Enum Medicine As Integer
-    A = 0
-    B = 2
-    C = 3
-End Enum
+﻿Namespace Enums
+    Public Enum Medicine As Integer
+        A = 0
+        B = 2
+        C = 3
+    End Enum
+End Namespace
