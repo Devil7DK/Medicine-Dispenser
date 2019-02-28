06174e973a6cf90b7439608f860c6ca2b2998f59
Devil7DK <dineshthangavel47@gmail.com>
2019-02-28 13:23:20 +0530

    Object: Medication - Add Namespace of Medicine

--- Medicine Dispenser - Windows Client/Classes/Objects/Medication.vb
+++ Medicine Dispenser - Windows Client/Classes/Objects/Medication.vb
@@ -2,7 +2,7 @@
     Public Class Medication
 
 #Region "Properties/Fields"
-        Property Medicine As Medicine
+        Property Medicine As Enums.Medicine
         Property Dosage As Integer
         Property Hour As Integer
         Property Minute As Integer
@@ -10,13 +10,13 @@
 
 #Region "Constructor"
         Sub New()
-            Me.Medicine = Medicine.A
+            Me.Medicine = Enums.Medicine.A
             Me.Dosage = 0
             Me.Hour = 0
             Me.Minute = 0
         End Sub
 
-        Sub New(ByVal Medicine As Medicine, ByVal Dosage As Integer, ByVal Hour As Integer, ByVal Minute As Integer)
+        Sub New(ByVal Medicine As Enums.Medicine, ByVal Dosage As Integer, ByVal Hour As Integer, ByVal Minute As Integer)
             Me.Medicine = Medicine
             Me.Dosage = Dosage
             Me.Hour = Hour
