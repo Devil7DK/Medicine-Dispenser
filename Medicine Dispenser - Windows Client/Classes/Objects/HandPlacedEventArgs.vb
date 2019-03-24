ff0f8020c7b690b207c10633bf42c1c7e7f94b38
vishunupriya7 <priyavishnu462@gmail.com>
2019-03-24 20:32:33 +0530

    Objects: Add HandPlaced Event Arg

---
+++ Medicine Dispenser - Windows Client/Classes/Objects/HandPlacedEventArgs.vb
@@ -0,0 +1,15 @@
+﻿Namespace Objects
+    Public Class HandPlacedEventArgs : Inherits EventArgs
+
+#Region "Properties"
+        Property isHandPlaced As Boolean
+#End Region
+
+#Region "Constructor"
+        Sub New(ByVal isHandPlaced As Boolean)
+            Me.isHandPlaced = isHandPlaced
+        End Sub
+#End Region
+
+    End Class
+End Namespace
