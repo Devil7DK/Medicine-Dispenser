d35e913991808ebc10f133c68393f108e21599ac
vishunupriya7 <priyavishnu462@gmail.com>
2019-03-24 20:37:27 +0530

    Project: Move Project to Sub Folder

---
+++ windows-client/Medicine Dispenser - Windows Client/Classes/Keys.vb
@@ -0,0 +1,5 @@
+﻿Public Class Keys
+
+    Public Shared LuxandFaceSDK As String = "hxVuGehGdNwqOug2HJmV/TiExhzRffPLbvlVzmf+vE09+KB44l72UvcRn91rcK8mlwBiOwoc1bKoSYIjsFmVJRYNr0MAPoTXxAcLOaTXIXTiEVauhXiNrQyWwFF+V8F9s/QTAnvKdg6YrQJMAOdATb+lSsVP1wRLKFzwDVHoMFE="
+
+End Class
