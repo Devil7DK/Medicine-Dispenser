fb1aed5c18f7fd4a5bbe233d5c24b9e6db6f0143
Devil7DK <dineshthangavel47@gmail.com>
2019-03-25 09:23:32 +0530

    Project: Add Splash

--- windows-client/Medicine Dispenser - Windows Client/My Project/Application.Designer.vb
+++ windows-client/Medicine Dispenser - Windows Client/My Project/Application.Designer.vb
@@ -34,5 +34,10 @@ Namespace My
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.MedicineDispenser.frm_Login
         End Sub
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+        Protected Overrides Sub OnCreateSplashScreen()
+            Me.SplashScreen = Global.MedicineDispenser.frm_Splash
+        End Sub
     End Class
 End Namespace
